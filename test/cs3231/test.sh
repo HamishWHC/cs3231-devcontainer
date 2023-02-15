6f39a1f188d7e0709b94e357cb4f389fb8a85273
HamishWHC <20359560+HamishWHC@users.noreply.github.com>
2023-02-15 11:09:23 +1100

    OS161 dev env.

--- test/cs3231/test.sh
+++ test/cs3231/test.sh
@@ -4,7 +4,8 @@ source test-utils.sh
 
 # Template specific tests
 check "distro" lsb_release -c
-check "greeting" [ $(cat /usr/local/etc/greeting.txt | grep hey) ]
+
+# TODO
 
 # Report result
 reportResults
